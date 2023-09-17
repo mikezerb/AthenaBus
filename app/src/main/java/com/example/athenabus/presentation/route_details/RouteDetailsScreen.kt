9687172854387f6b62c7ff7e84cb147c3d840342
mikezerb <mikeaposto@gmail.com>
2023-09-17 20:17:16 +0300

    added test topappbar with back arrow

--- app/src/main/java/com/example/athenabus/presentation/route_details/RouteDetailsScreen.kt
+++ app/src/main/java/com/example/athenabus/presentation/route_details/RouteDetailsScreen.kt
@@ -5,20 +5,29 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DirectionsBus
 import androidx.compose.material.icons.filled.Timeline
 import androidx.compose.material.icons.outlined.DirectionsBus
 import androidx.compose.material.icons.outlined.Timeline
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,12 +36,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.athenabus.R
 import com.example.athenabus.presentation.common.TabItem
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun RouteDetailsScreen(
     navController: NavController,
@@ -51,60 +66,111 @@ fun RouteDetailsScreen(
         )
     )
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        var selectedTabIndex by remember { mutableStateOf(0) }
-        val pagerState = rememberPagerState { tabItems.size }
-        LaunchedEffect(selectedTabIndex) {
-            pagerState.animateScrollToPage(selectedTabIndex)
-        }
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-        LaunchedEffect(pagerState.currentPage, pagerState.isScrollInProgress) {
-            if (!pagerState.isScrollInProgress) {    // for more than two pages
-                selectedTabIndex = pagerState.currentPage
-            }
 
-        }
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            TabRow(selectedTabIndex = selectedTabIndex) {
-                tabItems.forEachIndexed { index, tabItem ->
-                    Tab(
-                        selected = index == selectedTabIndex,
-                        onClick = { selectedTabIndex = index },
-                        text = { Text(text = tabItem.title) },
-                        icon = {
-                            Icon(
-                                imageVector = if (index == selectedTabIndex) {
-                                    tabItem.selectedIcon
-                                } else tabItem.unSelectedIcon,
-                                contentDescription = tabItem.title
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Column {
+                        Text(
+                            text = "Test Line 123",
+                            style = TextStyle(
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                letterSpacing = 0.15.sp
                             )
-                        }
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 4.dp),
+                            text = "ID: 1234",
+                            style = TextStyle(
+                                fontSize = 14.sp, // Adjust the font size as needed
+                                fontWeight = FontWeight.Normal,
+                                letterSpacing = 0.25.sp,
+                                color = MaterialTheme.colorScheme.secondary // Adjust the color as needed
+                            )
+                        )
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
 
-                    )
-                }
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { padding ->
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            var selectedTabIndex by remember { mutableStateOf(0) }
+            val pagerState = rememberPagerState { tabItems.size }
+            LaunchedEffect(selectedTabIndex) {
+                pagerState.animateScrollToPage(selectedTabIndex)
+            }
+
+            LaunchedEffect(pagerState.currentPage) {
+                // Used for more than two pages
+//                if (!pagerState.isScrollInProgress) {
+//                    selectedTabIndex = pagerState.currentPage
+//                }
+                selectedTabIndex = pagerState.currentPage
 
             }
-            HorizontalPager(
-                state = pagerState,
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) { index ->
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(text = tabItems[index].title)
-                    DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                TabRow(selectedTabIndex = selectedTabIndex) {
+                    tabItems.forEachIndexed { index, tabItem ->
+                        Tab(
+                            selected = index == selectedTabIndex,
+                            onClick = { selectedTabIndex = index },
+                            text = { Text(text = tabItem.title) },
+                            icon = {
+                                Icon(
+                                    imageVector = if (index == selectedTabIndex) {
+                                        tabItem.selectedIcon
+                                    } else tabItem.unSelectedIcon,
+                                    contentDescription = tabItem.title
+                                )
+                            }
 
+                        )
+                    }
+
+                }
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) { index ->
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(text = tabItems[index].title)
+                        DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+
+                        }
                     }
                 }
+
             }
 
         }
 
     }
+
 
 }
