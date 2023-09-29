6a60a6ac4976d6bcef162a5da0ce9acaef5097fa
mikezerb <mikeaposto@gmail.com>
2023-09-30 00:43:22 +0300

    remove minHeight line card

--- app/src/main/java/com/example/athenabus/presentation/bus_list/components/BusLineItem.kt
+++ app/src/main/java/com/example/athenabus/presentation/bus_list/components/BusLineItem.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,10 +18,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,14 +37,10 @@ fun BusLineItem(
     onItemClick: (Line) -> Unit,
     onToggleFavorite: (Line) -> Unit
 ) {
-    // Define a mutable state to track the star state
-    var isStarred by remember { mutableStateOf(false) }
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 4.dp)
-            .defaultMinSize(minHeight = 100.dp),
+            .padding(horizontal = 8.dp, vertical = 4.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
         ),
