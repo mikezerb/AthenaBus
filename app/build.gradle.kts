c22028500697016534530a2f86320845b99c31fc
mikezerb <mikeaposto@gmail.com>
2023-09-30 00:42:16 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.activity:activity-ktx:1.7.2")
@@ -83,8 +83,8 @@ dependencies {
     ksp("androidx.hilt:hilt-compiler:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
     // Compose dependencies
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha02")
-    implementation("androidx.navigation:navigation-compose:2.6.0-alpha08")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
     implementation("com.google.accompanist:accompanist-pager:0.27.1")
     // Coroutines
@@ -96,7 +96,7 @@ dependencies {
     //Accompanist
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.4-beta")
     //Compose Foundation
-    implementation("androidx.compose.foundation:foundation:1.6.0-alpha05")
+    implementation("androidx.compose.foundation:foundation:1.6.0-alpha06")
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
