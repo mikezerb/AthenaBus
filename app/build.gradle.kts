26d8fc88448537112e8dc83b3533062e919b72fd
mikezerb <mikeaposto@gmail.com>
2023-10-13 02:05:09 +0300

    latest material3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha09")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -75,7 +76,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha07")
-    implementation("androidx.compose.material3:material3:1.1.2")
     // Location services
     implementation("com.google.android.gms:play-services-location:21.0.1")
     // Maps
