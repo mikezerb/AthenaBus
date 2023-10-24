886fadb88b45acd7553cf56fe8d8d4ad78b7c634
mikezerb <mikeaposto@gmail.com>
2023-10-25 01:22:28 +0300

    change navigation animation to bus line item

--- app/src/main/java/com/example/athenabus/presentation/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/example/athenabus/presentation/navigation/HomeNavGraph.kt
@@ -2,7 +2,17 @@ package com.example.athenabus.presentation.navigation
 
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -36,7 +46,7 @@ fun HomeNavGraph(navController: NavHostController) {
     ) {
         navigation(
             startDestination = Route.NewBusLineScreen.route,
-            route = Route.NewBusLine.route
+            route = Route.NewBusLine.route,
         ) {
             composable(route = Route.NewBusLineScreen.route) {
                 NewBusLineListScreen(navController)
@@ -56,6 +66,7 @@ fun HomeNavGraph(navController: NavHostController) {
             navigation(
                 startDestination = Route.LineDetailActivityScreen.route,
                 route = Route.LineDetailsActivity.route
+
             ) {
                 composable(
                     route = Route.LineDetailActivityScreen.route,
@@ -64,7 +75,19 @@ fun HomeNavGraph(navController: NavHostController) {
                         navArgument(name = "lineCode") { type = NavType.StringType },
                         navArgument(name = "lineDesc") { type = NavType.StringType },
                         navArgument(name = "isFav") { type = NavType.BoolType },
-                    )
+                    ),
+                    enterTransition = {
+                        slideInVertically(
+                            initialOffsetY = { -40 }
+                        ) + expandVertically(
+                            expandFrom = Alignment.Top
+                        ) + scaleIn(
+                            transformOrigin = TransformOrigin(0.5f, 0f)
+                        ) + fadeIn(initialAlpha = 0.3f)
+                    },
+                    exitTransition = {
+                        slideOutVertically() + shrinkVertically() + fadeOut() + scaleOut(targetScale = 1.2f)
+                    }
                 ) { navBackStackEntry ->
                     LineDetailsScreen(
                         lineId = navBackStackEntry.arguments?.getString("lineId") ?: "",
@@ -75,7 +98,6 @@ fun HomeNavGraph(navController: NavHostController) {
                     )
                 }
             }
-
         }
     }
 }
